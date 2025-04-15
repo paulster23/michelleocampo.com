64d243fe8e305ac445d1f2e906b1af9e7140e899
Paul Sullivan <paulster23@gmail.com>
2025-04-15 18:40:28 -0400

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
         className="absolute inset-0 bg-cover bg-center"
         style={{
           backgroundImage:
-            "url('./dist/images/444copy.jpg?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')",
+            "url('./dist/images/444copy.jpg')",
         }}
       ></div>
       <div className="relative h-full flex flex-col justify-center items-center text-center text-white z-20 px-4">
