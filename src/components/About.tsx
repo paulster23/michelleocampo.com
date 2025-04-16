fb99e384da229b6111f9fee16a6e1690bbe3242e
Paul Sullivan <paulster23@gmail.com>
2025-04-15 20:44:04 -0400

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,7 +8,7 @@ const About = () => {
           <div className="md:w-1/2 mb-8 md:mb-0 md:pr-8">
             <div className="relative">
               <img 
-                src="https://github.com/paulster23/michelleocampo.com/blob/main/dist/images/michelleo.jpg?raw=true&&auto=format&fit=crop&w=1000&q=80" 
+                src="https://github.com/paulster23/michelleocampo.com/blob/main/dist/images/michelleocampo.jpg?raw=true&&auto=format&fit=crop&w=1000&q=80" 
                 alt="Michelle Ocampo" 
                 className="w-full h-auto rounded-lg shadow-lg"
               />
